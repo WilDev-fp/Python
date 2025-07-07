934c60f6dfe875c06701eac86ddf15053afce0b6
WilDev-fp <wiliscuola@gmail.com>
2025-07-07 02:30:09 +0200

    change connection from global to context

--- analisi_settore_economico_pesca_it/services/importanza_service.py
+++ analisi_settore_economico_pesca_it/services/importanza_service.py
@@ -3,10 +3,11 @@ import sqlite3
 
 class ImportanzaService:
     def __init__(self, db_path='../data/pesca.db'):
-        self.conn = sqlite3.connect(db_path)
+        self.db_path = db_path
 
     def media_perc_valore_aggiunto_per_area_anno(self):
-        df_imp = pd.read_sql_query("SELECT * FROM importanza", self.conn)
-        df_reg = pd.read_sql_query("SELECT * FROM regioni", self.conn)
-        df_imp = df_imp.merge(df_reg, left_on='regione', right_on='id_regione')
-        return df_imp.groupby(['anno', 'area'])['perc_valore_servizi'].mean().reset_index()
+        with sqlite3.connect(self.db_path) as conn:
+            df_imp = pd.read_sql_query("SELECT * FROM importanza", conn)
+            df_reg = pd.read_sql_query("SELECT * FROM regioni", conn)
+            df_imp = df_imp.merge(df_reg, left_on='regione', right_on='id_regione')
+            return df_imp.groupby(['anno', 'area'])['perc_valore_servizi'].mean().reset_index()
--- analisi_settore_economico_pesca_it/services/produttivita_service.py
+++ analisi_settore_economico_pesca_it/services/produttivita_service.py
@@ -3,14 +3,16 @@ import sqlite3
 
 class ProduttivitaService:
     def __init__(self, db_path='../data/pesca.db'):
-        self.conn = sqlite3.connect(db_path)
+        self.db_path = db_path
 
     def produttivita_per_area_anno(self):
-        df_prod = pd.read_sql_query("SELECT * FROM produttivita", self.conn)
-        df_reg = pd.read_sql_query("SELECT * FROM regioni", self.conn)
-        df_prod = df_prod.merge(df_reg, left_on='regione', right_on='id_regione')
-        return df_prod.groupby(['anno', 'area'])['produttivita_in_migliaia_euro'].sum().reset_index()
+        with sqlite3.connect(self.db_path) as conn:
+            df_prod = pd.read_sql_query("SELECT * FROM produttivita", conn)
+            df_reg = pd.read_sql_query("SELECT * FROM regioni", conn)
+            df_prod = df_prod.merge(df_reg, left_on='regione', right_on='id_regione')
+            return df_prod.groupby(['anno', 'area'])['produttivita_in_migliaia_euro'].sum().reset_index()
 
     def produttivita_nazionale_anno(self):
-        df_prod = pd.read_sql_query("SELECT * FROM produttivita", self.conn)
-        return df_prod.groupby('anno')['produttivita_in_migliaia_euro'].sum().reset_index()
+        with sqlite3.connect(self.db_path) as conn:
+            df_prod = pd.read_sql_query("SELECT * FROM produttivita", conn)
+            return df_prod.groupby('anno')['produttivita_in_migliaia_euro'].sum().reset_index()
